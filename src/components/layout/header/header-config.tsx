90a0e914314316e60a41ae8509fd478108342041
peterwallacekaranja <itsmeandyou108@gmail.com>
2025-03-08 15:16:53 +0300

    Update header-config.tsx

--- src/components/layout/header/header-config.tsx
+++ src/components/layout/header/header-config.tsx
@@ -42,27 +42,55 @@ export type TAccount = {
 
 export const platformsConfig: PlatformsConfig[] = [
     {
-        active: false,
+        active: true,
+        buttonIcon: <DerivBotLogo height={25} width={94} />,
+        description: localize('Automated trading at your fingertips. No coding needed.'),
+        href: 'https://bot.binaryfx.site',
+        icon: <DerivBotLogo height={32} width={121} />,
+        showInEU: false,
+    },
+    {
+        active: true,
         buttonIcon: <DerivTraderLogo height={25} width={114.97} />,
-        description: localize('A whole new trading experience on a powerful yet easy to use platform.'),
-        href: standalone_routes.trade,
+        description: localize('Trade with advanced tools and strategies.'),
+        href: 'https://trade.binaryfx.site',
         icon: <DerivTraderLogo height={32} width={148} />,
         showInEU: true,
     },
     {
         active: true,
-        buttonIcon: <DerivBotLogo height={25} width={94} />,
-        description: localize('Automated trading at your fingertips. No coding needed.'),
-        href: standalone_routes.bot,
-        icon: <DerivBotLogo height={32} width={121} />,
-        showInEU: false,
-    },
-    {
-        active: false,
         buttonIcon: <SmarttraderLogo height={24} width={115} />,
-        description: localize('Trade the world’s markets with our popular user-friendly platform.'),
-        href: standalone_routes.smarttrader,
+        description: localize('A user-friendly trading experience for all levels.'),
+        href: 'https://smarttrader.binaryfx.site',
         icon: <SmarttraderLogo height={32} width={153} />,
         showInEU: false,
+    },
+];
+
+export const TRADERS_HUB_LINK_CONFIG = {
+    as: 'a',
+    href: 'https://tradershub.binaryfx.site',
+    icon: <TradershubLogo iconSize='xs' />,
+    label: "Trader's Hub",
+};
+
+export const MenuItems: MenuItemsConfig[] = [
+    {
+        as: 'a',
+        href: 'https://reports.binaryfx.site',
+        icon: <ReportsLogo iconSize='xs' />,
+        label: localize('Reports'),
+    },
+    {
+        as: 'a',
+        href: 'https://cashier.binaryfx.site',
+        icon: <CashierLogo iconSize='xs' />,
+        label: localize('Cashier'),
+    },
+    {
+        as: 'a',
+        href: 'https://t.me/binaryfx_site',
+        icon: <CashierLogo iconSize='xs' />, // Change to a relevant Telegram icon
+        label: localize('Join our Telegram'),
     },
 ];
