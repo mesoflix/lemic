ca654dd0ce2c2a985d463d13b0c059288eec5517
peterwallacekaranja <itsmeandyou108@gmail.com>
2025-03-08 15:05:39 +0300

    Update index.tsx

--- src/components/layout/app-logo/index.tsx
+++ src/components/layout/app-logo/index.tsx
@@ -1,12 +1,12 @@
-import { useDevice } from '@deriv-com/ui';
+import { standalone_routes } from '@/components/shared';
+import { DerivLogo, useDevice } from '@deriv-com/ui';
 import './app-logo.scss';
 
 export const AppLogo = () => {
     const { isDesktop } = useDevice();
 
-    return isDesktop ? (
-        <a href="https://bot.binarylab.site" target="_blank" rel="noopener noreferrer">
-            <img src="/logo.png" alt="Your Logo" className="app-header__logo" />
-        </a>
-    ) : null;
+    if (!isDesktop) return null;
+    return (
+        <DerivLogo className='app-header__logo' href='/logo.png' target='_blank' variant='wallets' />
+    );
 };
