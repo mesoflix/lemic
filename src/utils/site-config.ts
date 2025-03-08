457f55cbdeaacaf3570ea07e4b5c2d25d1c24313
peterwallacekaranja <itsmeandyou108@gmail.com>
2025-03-08 03:21:19 +0300

    Update site-config.ts

--- src/utils/site-config.ts
+++ src/utils/site-config.ts
@@ -1,4 +1,4 @@
-export const website_domain = 'bot.binaryfx.site';
-export const website_name = 'Binaryfx';
+export const website_domain = 'bot.binarylab.site';
+export const website_name = 'Binarylab';
 export const default_title = website_name;
 export const TRACKING_STATUS_KEY = 'tracking_status';
