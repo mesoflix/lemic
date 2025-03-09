4debca9e9809976e218ebf41a9a054a62c0577d3
peterwallacekaranja <itsmeandyou108@gmail.com>
2025-03-09 21:08:05 +0300

    Update service-worker.ts

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,4 +1,4 @@
-const CACHE_NAME = "binaryfx-cache-v1";
+const CACHE_NAME = "binarylab-cache-v1";
 const urlsToCache = ["/", "/index.html", "/manifest.json"];
 
 self.addEventListener("install", (event: any) => {
